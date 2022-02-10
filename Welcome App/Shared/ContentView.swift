7fd3856832374ea1b115274465accf7a0fa95ebe
venkata prabhakar takkellapati <89261443+VenkataPrabhakar@users.noreply.github.com>
2022-02-10 12:08:29 -0600

    Add files via upload

---
+++ Welcome App/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Takkellapati,Venkata Prabhakar on 1/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Welcome App/Shared/Welcome_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Welcome_AppApp.swift
+//  Shared
+//
+//  Created by Takkellapati,Venkata Prabhakar on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Welcome_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
