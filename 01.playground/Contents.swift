cb0294c761df65c2dcc873efd65bf92e223ee4ff
venkata prabhakar takkellapati <89261443+VenkataPrabhakar@users.noreply.github.com>
2022-02-10 12:09:25 -0600

    Add files via upload

---
+++ 01.playground/Contents.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+var shoppingList = "The shopping list contains: "
+var foodItems = "Cheese, Butter, Chocolate Spread"
+var clothes = "Socks, T-shirts"
+
+if clothes.hasPrefix("Socks") {
+     print("The first item in clothes is socks")
+}else{
+    print("socks is not the first item in clothes")
+}
+print(foodItems.split(separator: ","))
+
+if clothes.contains(",") {
+print("Clothes contains more than one item")
+}else{
+    print("Clothes contain only one item")
+}
+
+foodItems[foodItems.startIndex..<foodItems.index(foodItems.endIndex,offsetBy: -7)]
+
+shoppingList +=
+foodItems[foodItems.index(foodItems.startIndex, offsetBy:
+8)..<foodItems.endIndex]
+clothes.remove(at: clothes.firstIndex(of: "T")!)
+
+clothes.remove(at: clothes.firstIndex(of: "-")!)
+print("\(shoppingList), \(clothes)")
+clothes.insert(contentsOf: ", Trousers", at:
+clothes.endIndex)
+var firstIndexOfR = shoppingList.index(after:
+shoppingList.firstIndex(of: "r")!)
+print(shoppingList[..<firstIndexOfR])
+
+//_______STRINGS___________________________________________________________________
+
+let password = "Demo@123"
+let confirmPassword = "Dem@123"
+if password == confirmPassword {
+    print("Password matches")
+}else{
+    print("Passwords doesn't matches")
+}
+
+let currentYear = "2021"
+var enteredYear = "2021"
+if currentYear.elementsEqual(enteredYear) == true
+{
+    print("Entered year matches with current year")
+}
+else{
+    print("Entered year does not match with current year")
+}
+var vehiclePoweredBy = "Diesel"
+ if vehiclePoweredBy != "Electricity" {
+ print("Vehicle is not eco-friendly")
+ }else{
+ print("Vehicle is eco-friendly")
+ }
+let nameGiven = "Anthony Martial"
+var enteredName = "ANTHONY MARTIAL"
+if nameGiven.lowercased() == enteredName.lowercased(){
+print("Entered name and given name matches with each other")
+}else{
+print("Entered name does not matches with the given name")
+}
