877c9fce46ebebe991739e9b4616efdebb61ff2a
venkata prabhakar takkellapati <89261443+VenkataPrabhakar@users.noreply.github.com>
2022-03-24 12:07:17 -0500

    Add files via upload

---
+++ StudentCourse/StudentCourseUITests/StudentCourseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StudentCourseUITestsLaunchTests.swift
+//  StudentCourseUITests
+//
+//  Created by Takkellapati,Venkata Prabhakar on 3/24/22.
+//
+
+import XCTest
+
+class StudentCourseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
